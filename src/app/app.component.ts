a2a0e377b207a273b68aa56cbd8849cbb64bce4d
codexshell <ericoderito@gmail.com>
2023-07-28 19:06:09 +0300

    chore: add basic seo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 import { GithubService } from './github.service';
 
@@ -11,9 +12,24 @@ export class AppComponent implements OnInit {
   title = 'gh-portfolio';
   username: string | undefined;
 
-  constructor(private githubService: GithubService) {}
+  constructor(
+    private githubService: GithubService,
+    private titleService: Title,
+    private meta: Meta
+  ) {}
 
   ngOnInit(): void {
     this.username = this.githubService.username;
+    this.titleService.setTitle('Github Portfolio app');
+    this.meta.addTags([
+      {
+        name: 'description',
+        content: `${this.username}'s Github portfolio`,
+      },
+      {
+        name: 'author',
+        content: this.username,
+      },
+    ]);
   }
 }
