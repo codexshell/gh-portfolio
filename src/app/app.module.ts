c8513b995a2dda56fc9f991d4e56ebf1d0ec1f0d
codexshell <ericoderito@gmail.com>
2023-07-28 18:49:40 +0300

    feat: enable server side rendering with `Angular Universal`

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 
 import { AppComponent } from './app.component';
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
@@ -9,8 +10,14 @@ import { RepositoriesComponent } from './repositories/repositories.component';
 import { OrganizationsComponent } from './organizations/organizations.component';
 
 @NgModule({
-  declarations: [AppComponent, PersonalInfoComponent, PanelComponent, RepositoriesComponent, OrganizationsComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    PersonalInfoComponent,
+    PanelComponent,
+    RepositoriesComponent,
+    OrganizationsComponent,
+  ],
+  imports: [BrowserModule, HttpClientModule, TransferHttpCacheModule],
   providers: [],
   bootstrap: [AppComponent],
 })
