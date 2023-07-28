a9e95505934702563dd7e1a08e9e63e4d7695e2f
codexshell <ericoderito@gmail.com>
2023-07-28 16:43:07 +0300

    feat: diplay personal information

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { GithubService } from './github.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gh-portfolio';
-  username = 'codexshell';
+  username: string | undefined;
+
+  constructor(private githubService: GithubService) {}
+
+  ngOnInit(): void {
+    this.username = this.githubService.username;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PersonalInfoComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
